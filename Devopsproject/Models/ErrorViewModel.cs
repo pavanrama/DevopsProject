b2f40c32c737fa1e2e6d3fb92e4444097507476e
Pavan Rama <pavan@Pavans-MBP.lan>
2024-02-02 15:45:16 -0600

    New Project

---
+++ Devopsproject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Devopsproject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
